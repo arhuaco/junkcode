cd6014891100929d36dde4cff465d59c4a41de3b
Nelson Castillo <nelsoneci@gmail.com>
2014-03-09 16:59:56 -0500

    Difficulty in binary.

---
+++ junk/bitcoin/difficulty_binary.py
@@ -0,0 +1,31 @@
+import hashlib
+import random
+import sys
+import time
+
+def get_random_nonce():
+  return str(random.random()) + '-' + str(random.random())
+
+def compute_difficulty(n_zeros):
+  return eval("0b" + "0" * n_zeros + "1" * (256 - n_zeros))
+
+def find_hash(text, difficulty):
+  while True:
+    m = hashlib.sha256()
+    m.update(text + ' ')
+    m.update(get_random_nonce())
+    if int(m.hexdigest(), 16) < difficulty:
+      return m.hexdigest()
+
+def hash_n(n_times, text):
+  """compute n times to get average time - return one hash"""
+  time_start = time.time()
+  for i in range(n_times):
+    h = find_hash('Gloria', int_diff)
+  total_time = time.time() - time_start
+  return h, total_time / n_times
+
+for diff in range(0, 257):
+  int_diff = compute_difficulty(diff)
+  h, total_time = hash_n(10, 'Gloria')
+  print diff, total_time, h
