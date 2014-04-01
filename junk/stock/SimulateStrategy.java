ba3204e7a734b47306007fd8961d925e8eeda9b4
Nelson Castillo <nelsoneci@gmail.com>
2014-04-01 17:11:40 -0500

    Basic simulator works.

--- junk/stock/SimulateStrategy.java
+++ junk/stock/SimulateStrategy.java
@@ -1,13 +1,13 @@
 import java.util.Random;
 
 /**
- * @author Nelson Castillo.
+ * @author Nelson Castillo -- nelsoneci@gmail.com
  */
 
 class SimulateStrategy
 {
     /**
-    * Simulate the life of an account with the given parameters - Rough simplification.
+    * Simulate the life of a trading account with the given parameters - rough simplification.
     * You should call this function many times and average the results.
     * @param maxSteps Maximum number of steps of the simulation.
     * @param startingCapital Starting amount of (say) USD dollars.
@@ -15,31 +15,48 @@ class SimulateStrategy
                         not care about having more money.
     * @param winningProbability Is a number in the range [0, 1].
                                 The losing probability will be 1.0 - winningProbability.
-    * @param winningPercent How much of the account will we win if we do win? Range [0, 100]
+    * @param winningPercent How much of the account will we earn if we do win? Range [0, 100]
+                            The losing percentage will be 100 - winningPercent.
+    * @param losingPercent How much of the account will we lose if we do lose? Range [0, 100]
                             The losing percentage will be 100 - winningPercent.
     */
 
-    static double Simulate (int maxSteps,
+    static double Simulate (Random rand, int maxSteps,
                             double startingCapital, double maxCapital, double winningProbability,
-                            double winningPercent)
+                            double winningPercent, double losingPercent)
     {
         int i = 0;
 
         double capital = startingCapital;
 
-        while (i < maxSteps) {
+        for (; i < maxSteps; ++i) {
             if (capital <= 0.0) {
                 return 0.0;  // We lost all the money.
             }
             if (capital >= maxCapital) {
                 return maxCapital;  // We are rich, baby. We don't need more money.
             }
+
+            if (rand.nextFloat() < winningProbability) {
+                capital *= 1.0 + winningPercent / 100.0;
+            } else {
+                capital *= 1.0 - losingPercent / 100.0;
+            }
         }
 
-        return 0;
+        return capital;
     }
 
     public static void main (String args[]) throws java.io.IOException
     {
+        Random rand = new Random();
+        int ntimes = 30000;
+        for (double winningProb = 0.3; winningProb <= 0.7; winningProb += 0.01) {
+            double sum = 0.0;
+            for (int i = 0; i < ntimes; ++i) {
+                sum += Simulate(rand, 272, 10000, 1000000, winningProb, 1.0, 2.0);
+            }
+            System.out.printf("%f %.2f\n", winningProb, sum / ntimes);
+        }
     }
 }
