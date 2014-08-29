120b24e50062394210a57ced88ed173b3236498d
Nelson Castillo <nelsoneci@gmail.com>
2014-08-29 01:18:40 -0500

    Moved to python3. Reused code from another module.

--- pettycoin_test/test_server.py
+++ pettycoin_test/test_server.py
@@ -1,38 +1,31 @@
-#!/usr/bin/env python
-
-from __future__ import absolute_import, division, print_function, unicode_literals
-if sys.version_info  < (3,0):
-    range = xrange
+''' Server used for testing.  '''
 
 import json
-import SocketServer
-import random
+import socketserver
 import sys
 import time
 
+import json_generator
+
 MAX_LEN = 1024 * 10
+HOST = 'localhost'
+PORT = 2020
 
-def make_random_json(max_depth=3, max_keys=3):
-    ret = {}
-    for idx in range(random.randint(1, max_keys)):
-      assert max_depth - 1 >= 0
-      next_depth = random.randint(0, max_depth - 1)
-      if next_depth > 0:
-        ret[str(idx)] = make_random_json(next_depth)
-      else:
-        ret[str(idx)] = idx
-    return ret
-
-class TestingServer(SocketServer.BaseRequestHandler):
-
+class TestingRequestHandler(socketserver.BaseRequestHandler):
+    '''' The request handler. '''
     def handle(self):
         time.sleep(2)
-        for i in range(10):
-            to_send = json.dumps(make_random_json())
+        for _ in range(10):
+            to_send = json.dumps(json_generator.make_random_json_like_hash())
             print(to_send, file=sys.stderr)
             self.request.sendall(to_send)
 
-if __name__ == "__main__":
-    SocketServer.TCPServer.allow_reuse_address = True
-    server = SocketServer.TCPServer(('localhost', 2020), TestingServer)
+def main():
+    ''' Our main function. '''
+    socketserver.TCPServer.allow_reuse_address = True
+    server = socketserver.TCPServer((HOST, PORT), TestingRequestHandler)
     server.serve_forever()
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
