0d0326010150f634576639ba9d5dd6c102866106
Nelson Castillo <nelsoneci@gmail.com>
2014-11-06 22:13:00 -0500

    Improved.

--- CA/opencl/ca.py
+++ CA/opencl/ca.py
@@ -17,15 +17,16 @@ mf = cl.mem_flags
 a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a_np)
 b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b_np)
 
+
 prg = cl.Program(ctx, """
-__kernel void sum(__global const int *a_g, __global const int *b_g, __global int *res_g) {
+__kernel void sum(__global const int *a_g, __global const int *b_g, __global int *res_g, const int N) {
   int gid = get_global_id(0);
-  res_g[gid] = a_g[gid] + b_g[gid];
+  res_g[gid] = a_g[gid] + b_g[gid] + gid;
 }
 """).build()
 
 res_g = cl.Buffer(ctx, mf.WRITE_ONLY, a_np.nbytes)
-prg.sum(queue, a_np.shape, None, a_g, b_g, res_g)
+prg.sum(queue, a_np.shape, None, a_g, b_g, res_g, np.int32(N))
 
 # FIX
 #res_np = np.empty_like(a_np)
