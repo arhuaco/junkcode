0101ff61dfb4a55df1ebed140242b001eb1e1936
Nelson Castillo <nelsoneci@gmail.com>
2015-04-21 00:37:04 -0500

    Fix obvious PEP8 offences. It works now.

---
+++ junk/xcp/create_wallet_test/xcp_gen_counterwallet_addresses.py
@@ -0,0 +1,44 @@
+from word_list import mnemonic_word_list
+
+from binascii import unhexlify
+import bitcoin
+import math
+
+def mMod(a, b):
+    return int(a  - math.floor(a/b)*b)
+
+def toHex(s):
+    seed = ''
+    for i in s:
+        seed += ('0'*8 + format(i, 'x'))[-8:]
+    return seed
+
+def makeSeed(phrase, words=None):
+    if words is None:
+        words = mnemonic_word_list
+    n = len(words)
+    s = []
+    for i in range(4):
+        w1 = words.index(phrase[3*i])
+        w2 = words.index(phrase[3*i + 1])
+        w3 = words.index(phrase[3*i + 2])
+        s.append(w1 + n*mMod(w2-w1, n) + n*n*mMod(w3 - w2, n))
+    return s
+
+def GetKeys(pass_phrase, depth):
+    step1 = makeSeed(pass_phrase)
+    step2 = toHex(step1)
+    step4 = unhexlify(step2)
+    master = bitcoin.bip32_master_key(step4)
+    external = bitcoin.bip32_ckd(master, 2**31 + 0) # hardened
+    external = bitcoin.bip32_ckd(external, 0)
+    ret = []
+    for idx in range(depth):
+        priv_32 = bitcoin.bip32_ckd(external, idx)
+        priv = bitcoin.bip32_extract_key(priv_32)
+        pub = bitcoin.privtopub(priv)
+        ret.append(bitcoin.pubtoaddr(pub))
+    return ret
+
+print(' '.join(mnemonic_word_list[:12]))
+print(GetKeys(mnemonic_word_list[:12], 2))
