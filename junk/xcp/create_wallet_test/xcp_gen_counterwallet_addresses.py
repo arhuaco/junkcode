beb5c0fd99481563c71bb7b7eadeb7420ecdb57b
Nelson Castillo <nelsoneci@gmail.com>
2015-04-23 01:22:26 -0500

    Improved the demo. Now it works for testnet/mainnet.

--- junk/xcp/create_wallet_test/xcp_gen_counterwallet_addresses.py
+++ junk/xcp/create_wallet_test/xcp_gen_counterwallet_addresses.py
@@ -12,18 +12,20 @@ from word_list import mnemonic_word_list
 
 from binascii import unhexlify
 import bitcoin
+from Crypto import Random
 import math
+import struct
 
-def mMod(a, b):
+def _mMod(a, b):
     return int(a  - math.floor(a/b)*b)
 
-def toHex(s):
+def _toHex(s):
     seed = ''
     for i in s:
         seed += ('0'*8 + format(i, 'x'))[-8:]
     return seed
 
-def makeSeed(phrase, words=None):
+def _makeSeed(phrase, words=None):
     if words is None:
         words = mnemonic_word_list
     n = len(words)
@@ -32,14 +34,15 @@ def makeSeed(phrase, words=None):
         w1 = words.index(phrase[3*i])
         w2 = words.index(phrase[3*i + 1])
         w3 = words.index(phrase[3*i + 2])
-        s.append(w1 + n*mMod(w2-w1, n) + n*n*mMod(w3 - w2, n))
+        s.append(w1 + n*_mMod(w2-w1, n) + n*n*_mMod(w3 - w2, n))
     return s
 
-def GetKeys(pass_phrase, depth):
-    step1 = makeSeed(pass_phrase)
-    step2 = toHex(step1)
+def get_keys(pass_phrase, depth, testnet):
+    step1 = _makeSeed(pass_phrase)
+    step2 = _toHex(step1)
     step4 = unhexlify(step2)
-    master = bitcoin.bip32_master_key(step4)
+    vbytes = vbytes = bitcoin.TESTNET_PRIVATE if testnet else bitcoin.MAINNET_PRIVATE
+    master = bitcoin.bip32_master_key(step4, vbytes)
     external = bitcoin.bip32_ckd(master, 2**31 + 0) # hardened
     external = bitcoin.bip32_ckd(external, 0)
     ret = []
@@ -47,8 +50,25 @@ def GetKeys(pass_phrase, depth):
         priv_32 = bitcoin.bip32_ckd(external, idx)
         priv = bitcoin.bip32_extract_key(priv_32)
         pub = bitcoin.privtopub(priv)
-        ret.append(bitcoin.pubtoaddr(pub))
+        ret.append(bitcoin.pubtoaddr(pub, 111 if testnet else 0))
     return ret
 
-print(' '.join(mnemonic_word_list[:12]))
-print(GetKeys(mnemonic_word_list[:12], 2))
+def random_passphrase():
+    words = []
+    for _ in range(12):
+        idx = struct.unpack("<L", Random.get_random_bytes(4))[0] % len(mnemonic_word_list)
+        words.append(mnemonic_word_list[idx])
+    return words
+
+def demo_generate(testnet):
+    words = random_passphrase()
+    network = 'testnet' if testnet else 'mainnet'
+    print('Generating wallet for {}. passphrase: {}'.format(network, ' '.join(words)))
+    print(get_keys(words, 1, testnet))
+
+def main():
+    demo_generate(testnet=False)
+    demo_generate(testnet=True)
+
+if __name__ == '__main__':
+    main()
