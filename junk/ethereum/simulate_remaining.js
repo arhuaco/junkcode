63d66779fa26ccadeb01f51e86de9ed6b2c96321
Nelson Castillo <nelsoneci@gmail.com>
2015-08-06 04:23:16 -0500

    Here it goes the simulation.

---
+++ junk/ethereum/simulate_remaining.js
@@ -0,0 +1,39 @@
+function Simulate(){
+    var latestBlock = eth.getBlock("latest");
+    var latest = latestBlock.number;
+
+    var n_inc = 0;
+    var n_dec = 0;
+
+    console.log('Looking back 10 minutes (Approx 40 blocks)');
+    lookback = 40;
+    var prev = eth.getBlock(latest - lookback).gasLimit
+    var current;
+    for (i = latest - lookback + 1; i <= latest; i++){
+        current = eth.getBlock(i).gasLimit;
+        if (current > prev) {
+            ++n_inc;
+        } else {
+            ++n_dec;
+        }
+     }
+
+    console.log('Voting up: ' + n_inc);
+    console.log('Votoing down: ' + n_dec);
+    console.log('Simulating... Using a line would be enough.');
+
+    var blocks_remaining = 0;
+    while (current < 21000) {
+        var delta = current / 1024;
+        if (Math.random() < n_dec / n_inc) {
+            current = current - delta;
+        } else {
+            current = current + delta;
+        }
+        current = Math.max(5000, current);
+        blocks_remaining += 1;
+    }
+    console.log('Blocks remaining: ' + blocks_remaining);
+    console.log('Hours remaining: ' + blocks_remaining / 4 / 60);
+}
+Simulate()
