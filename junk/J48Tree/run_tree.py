35563d705e3b81f8c229018db6392e322091eec1
Nelson Castillo <nelsoneci@gmail.com>
2015-12-02 11:27:16 -0500

    Fix.

--- junk/J48Tree/run_tree.py
+++ junk/J48Tree/run_tree.py
@@ -1,9 +1,13 @@
 ' Run a decision tree. Python 2.7! Legacy code... '
 
 import pydot
+import csv
 
 def main():
     tree = pydot.graph_from_dot_data(open('ModeloB.dot').read())
+    with open('total.csv') as csv_file:
+         for row in csv.reader(csv_file):
+             print ', '.join(row)
 
 if __name__ == "__main__":
     # execute only if run as a script.
