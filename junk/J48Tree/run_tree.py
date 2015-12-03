9749a57519f4a7eb285aa1449068ef91221ffed2
Nelson Castillo <nelsoneci@gmail.com>
2015-12-03 17:52:36 -0500

    Improved.

--- junk/J48Tree/run_tree.py
+++ junk/J48Tree/run_tree.py
@@ -4,14 +4,22 @@ import pydot
 import csv
 import pprint
 
-def main():
-    with open('total.csv') as csv_file:
+def process(file_name):
+    with open(file_name) as csv_file:
+        for the_header in csv.reader(csv_file, delimiter=';'):
+            break # Saving header. TODO(nel): Is there a better way to do this?
+
+    with open(file_name) as csv_file:
         reader = csv.DictReader(csv_file, delimiter=';')
         result = {}
         for row in reader:
             for column, value in row.iteritems():
                 result.setdefault(column, []).append(value)
-    pprint.pprint(result)
+    writer = csv.DictWriter(open('out.txt', 'w'), fieldnames=the_header)
+    writer.writeheader()
+
+def main():
+    process('total.csv')
 
 if __name__ == "__main__":
     # execute only if run as a script.
