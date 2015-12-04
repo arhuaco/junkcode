c6584246a34abd9a34d9995e2e9dbcb51e442b5b
Nelson Castillo <nelsoneci@gmail.com>
2015-12-03 21:42:57 -0500

    Expressions almost there.

--- junk/J48Tree/run_tree.py
+++ junk/J48Tree/run_tree.py
@@ -4,17 +4,38 @@ import pydot
 import csv
 import pprint
 
-def process(file_name):
-    with open(file_name) as csv_file:
+def resolve_variable(node_or_edge):
+    ' Get the value of a variable of the model. '
+    var_name = node_or_edge.get_label()[1:-1]
+    return var_name
+
+def eval_tree(tree, inputs, root_node_name):
+    root_val  = resolve_variable(tree.get_node(root_node_name)[0])
+    for edge in [e for e in tree.get_edges() if e.get_source() == root_node_name]:
+        #print edge.get_destination()
+        #print edge.get_label()
+        print 'exp', root_val + resolve_variable(edge)
+    #print edge
+    #print root, edge
+
+def process_inputs(data_file, graph_file, root_node_name):
+    ' Read a decision graph and a CSV file with values for tree evaluation. '
+    # First, read the variables open(data_file) as csv_file:
+    with open(data_file) as csv_file:
         reader = csv.DictReader(csv_file, delimiter=';')
-        result = {}
+        inputs  = {}
         for row in reader:
             for column, value in row.iteritems():
-                result.setdefault(column, []).append(value)
-    pprint.pprint(result)
+                inputs.setdefault(column, []).append(value)
+        # Read the graph.
+        tree = pydot.graph_from_dot_data(open(graph_file).read())
+        # Evaluate the graph
+        result = eval_tree(tree=tree, inputs=inputs, root_node_name=root_node_name)
 
 def main():
-    process('total.csv')
+    process_inputs(data_file='total.csv',
+                   graph_file='ModeloB.dot',
+                   root_node_name='N0')
 
 if __name__ == "__main__":
     # execute only if run as a script.
