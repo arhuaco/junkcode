0c7e0c58c0c1370321aae9464d514fd39cff1ea3
Nelson Castillo <len@zsum.net>
2020-04-12 00:28:57 -0500

    Improve example for lambda comparison.

--- junk/c++17/lambda_map_comparison.cc
+++ junk/c++17/lambda_map_comparison.cc
@@ -4,12 +4,14 @@
 
 int main() {
   // Using a lambda for comparisons.
-  auto comparator = [](const std::string& c1, const std::string& c2) -> bool {
-    return c1 < c2;
+  auto my_gt = [](const std::string& c1, const std::string& c2) -> bool {
+    return std::stod(c1) > std::stod(c2);
   };
-  std::map<std::string, int, decltype(comparator)> mymap(comparator);
-  mymap = {{"1", 0}, {"9", 1}, {"4", 2}, {"3", 3}};
+  // Note decltype and constructor call.
+  std::map<std::string, int, decltype(my_gt)> mymap(my_gt);
+  mymap = {{"2", 0}, {"9", 1}, {"00", 100}, {"0001", 1}, {"0001000", -1}};
+  // Not the same if you sort in lexicographic order.
   for (const auto& [key, value] : mymap)
-    std::cout << key << " " << value << std::endl;
+    std::cerr << key << " " << value << std::endl;
   return 0;
 }
