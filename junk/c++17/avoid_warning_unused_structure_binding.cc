cd82e14f381b8b9d97d82a19be5d0bef94e06894
Nelson Castillo <nelsoneci@gmail.com>
2020-04-26 12:37:20 -0500

    Another way to avoid warnings.

--- junk/c++17/avoid_warning_unused_structure_binding.cc
+++ junk/c++17/avoid_warning_unused_structure_binding.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <map>
 
+// Anothr way.
+// template<class T> void _unused(T &v) {do { (void)(v); } while (0); }
+
 int main(int argc, char *argv[]) {
   std::map<char, int> my_map = {{'b', 2}, {'a', 1}, {'c', 3}};
   // Avoid warning for unused 'v'. Not pretty.
