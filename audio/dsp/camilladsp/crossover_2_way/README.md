3322c827516d0dfb4de74d25e9868a654737ce13
Nelson Castillo <nelsoneci@gmail.com>
2024-05-19 02:24:33 -0500

    Backup audio recipies so that I don't lose them.
     2-way crossover with Camilladsp
     Short pipewrite recording tip.

---
+++ audio/dsp/camilladsp/crossover_2_way/README.md
@@ -0,0 +1,1 @@
+A crossover for a 2-way. I used Debian 12 (Bookworm) for this test.
---
+++ audio/dsp/camilladsp/crossover_2_way/run.sh
@@ -0,0 +1,2 @@
+#chrt -f 40 camilladsp -g-40 -p 1234 ~/camilladsp/configs/cross.yml -v -s camilladsp/statefile.ym -w
+chrt -f 40 camilladsp -g-40 -p 1234 ~/camilladsp/configs/cross.yml  -s camilladsp/statefile.ym -w
