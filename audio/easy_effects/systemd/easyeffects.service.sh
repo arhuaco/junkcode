067e4ed94562b9bc87e2dad1f393e27ace7705e7
Nelson Castillo <nelsoneci@gmail.com>
2025-05-19 04:27:19 -0500

    Fixes to scripts

--- audio/easy_effects/systemd/easyeffects.service.sh
+++ audio/easy_effects/systemd/easyeffects.service.sh
@@ -6,7 +6,7 @@ if [ "$1" = "start" ]; then
 	Xvfb :43 -screen 0 1024x768x16 &
 	sleep 3
 	export DISPLAY=:43
-	nohup easyeffects --gapplication-service  2>/dev/null >/dev/null &
+	easyeffects --gapplication-service
 fi
 
 if [ "$1" = "stop" ]; then
