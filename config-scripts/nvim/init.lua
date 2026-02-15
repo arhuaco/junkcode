90d68c635e6ef7aacafd114ec932daf40acf3bf6
Nelson Castillo <nelsoneci@gmail.com>
2026-02-15 07:19:10 -0500

    Init neovim

---
+++ config-scripts/nvim/init.lua
@@ -0,0 +1,5 @@
+vim.keymap.set("", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("", "<down>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
+vim.opt.mouse = ""
